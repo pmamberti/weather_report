02c2269b843938a88c7decb472c7531201531820
Piero Mamberti <piero.mamberti@gmail.com>
2021-04-10 19:05:11 +0100

    switch to flag.NewFlagSet

--- cli/cli.go
+++ cli/cli.go
@@ -3,19 +3,7 @@ package cli
 import (
 	"errors"
 	"flag"
-	"os"
 )
-
-var c, u *string
-
-func init() {
-	c = flag.String("city", "london", "enter city - required")
-	u = flag.String(
-		"unit",
-		"metric",
-		"pick one between metric and standard",
-	)
-}
 
 func Allowed(list []string, u string) bool {
 	for _, s := range list {
@@ -27,18 +15,21 @@ func Allowed(list []string, u string) bool {
 }
 
 func Parse(cmd []string) ([]string, error) {
-	os.Args = cmd
-	allowedUnits := []string{"metric", "standard", "imperial"}
 	var err error
-	flag.Parse()
+	arguments := flag.NewFlagSet("args", flag.ContinueOnError)
+	c := arguments.String("city", "london", "Required. Target city")
+	u := arguments.String("unit", "metric", "Optional. Temperature unit: Standard, Metric (default) or Imperial.")
+	arguments.Parse(cmd[1:])
+
 	if *c == "" {
-		err = errors.New("city not provided, you must specify one")
+		err = errors.New("city cannot be empty - default to london")
+		arguments.PrintDefaults()
+		// os.Exit(1)
 	}
-	if !Allowed(allowedUnits, *u) || *u == "" {
-		err = errors.New(
-			"measure unit not allowed, defaulting to metric ",
-		)
+
+	if !Allowed([]string{"metric", "standard", "imperial"}, *u) {
 		*u = "metric"
+		err = errors.New("unit must be metric standard or imperial")
 	}
 
 	return []string{*c, *u}, err
